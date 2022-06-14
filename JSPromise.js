d780f396b80c6d3748c22d483a927564dcb2c1c7
VidhyaPandiarajan <105809504+VidhyaPandiarajan@users.noreply.github.com>
2022-06-14 17:56:13 -0500

    Add files via upload

--- JSPromise.js
+++ JSPromise.js
@@ -1,16 +1,34 @@
+
 function getusers() {
+    let users = [];
     return new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve([
+        
+        users = [
             {username: 'steve', email:'steve@mail.com'},
             {username: 'dustin', email: 'dustin@mail.com'},
-        ]);
+        ];
+        if(users != null)
+        {
+            resolve(users);
+        }else{
+            reject("data failed");
+        }
+    
     }, 1000);
     });
     }
-function onFulfilled(users) {
-    console.log(users);
-}
 
-const promise = getusers();
-promise.then(onFulfilled);
+    const promise = getusers();
+    function finduser(username)  {
+        promise.then((value) => {
+  
+                const user = value.find((user) => user.username === username);
+                console.log(user);
+
+                
+            });
+
+        };
+        finduser('steve');
+    
